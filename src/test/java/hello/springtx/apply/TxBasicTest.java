da1640bbec6bba9ceba13379abfe07e408f686b1
HO2AST <lucid7@hotmail.co.kr>
2022-10-09 00:52:45 +0900

    트랜잭션 적용확인

---
+++ src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -0,0 +1,67 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+    @Autowired BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class={}", basicService.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @Test
+    void bean() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TxApplyBasicConfig.class);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("applicationContext.getBean(beanDefinitionName) = " + applicationContext.getBean(beanDefinitionName));
+        }
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
